0daef448962404ae0ff884a41382a9de9864a8d5
Jarno Varis <jarno.varis@gmail.com>
2023-01-01 12:53:28 +0200

    added cypress test for navigating to ivysaur-page

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -4,4 +4,11 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('navigating to another pokemon page works', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+    cy.contains('overgrow')
+  })
 })
